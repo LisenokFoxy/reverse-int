ea11b1ddd3fc88c1f2176607258b782069827a7c
Tatyana Pashintseva <Tatyana.Pashintseva@kcell.kz>
2020-02-23 21:24:39 +0600

    feat: prepare task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
   
+  let num=n.toString().split("").reverse().join("");
+  if (num.endsWith('-')) {
+      num= '-' + num;
+      return parseInt(num);
+  }
+  return parseInt(num);
 }
